fd97ccf3fab5af0c5e091c23b69258ed71364457
dontEatMe <lvlbgame@gmail.com>
2024-03-29 10:29:43 +0300

    dependencies to latest

--- src/Gui.js
+++ src/Gui.js
@@ -9,7 +9,7 @@ import RadioButton from './RadioButton.js';
 import Label from './Label.js';
 import ScrollBar from './ScrollBar.js';
 
-const VERSION = '0.3.11';
+const VERSION = '0.3.12';
 
 const raycaster = new THREE.Raycaster();
 
