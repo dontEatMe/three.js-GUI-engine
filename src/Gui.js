42b044478ee475fae1726bcf02d23f51512411fc
dontEatMe <lvlbgame@gmail.com>
2024-04-26 12:24:24 +0300

    fix for npm publishing

--- src/Gui.js
+++ src/Gui.js
@@ -9,7 +9,7 @@ import RadioButton from './RadioButton.js';
 import Label from './Label.js';
 import ScrollBar from './ScrollBar.js';
 
-const VERSION = '0.4.6';
+const VERSION = '0.4.7';
 
 const raycaster = new THREE.Raycaster();
 
