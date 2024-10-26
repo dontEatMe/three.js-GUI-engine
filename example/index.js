f54633700995ae24debce186eaa26860f381a4f9
dontEatMe <lvlbgame@gmail.com>
2024-10-26 12:23:05 +0300

    version 0.5.1

--- example/index.js
+++ example/index.js
@@ -109,7 +109,7 @@ checkbox2.onmouseover = function(object) { this.changeColor(0x668866); }
 checkbox2.onmouseout = function(object) { this.changeColor(0x88aa88); }
 interfaceScene.add(checkbox2);
 
-const editbox1 = new GUI.EditBox( {threeFont: font, placeholder: 'editbox'} );
+const editbox1 = new GUI.EditBox( {threeFont: font, placeholder: 'editbox', text: 'ABCDEFGHIGKLMNOP'} );
 editbox1.position.set( 0, 50, 0 );
 interfaceScene.add(editbox1);
 
