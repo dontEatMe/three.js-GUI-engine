7b3b0140e4089d78a6eb5ffe8e3e18c02e18dabb
dontEatMe <lvlbgame@gmail.com>
2025-02-01 14:07:11 +0300

    v0.5.13

--- src/Gui.js
+++ src/Gui.js
@@ -9,7 +9,7 @@ import { RadioButton, RADIOBUTTON_BASE, RADIOBUTTON_TEXT } from './RadioButton.j
 import { Label, LABEL_TEXT } from './Label.js';
 import { ScrollBar, SCROLLBAR_SLIDER, SCROLLBAR_UP, SCROLLBAR_DOWN } from './ScrollBar.js';
 
-const VERSION = '0.5.12';
+const VERSION = '0.5.13';
 
 const raycaster = new THREE.Raycaster();
 
